2d180119ae1b404fcdaa10f3e68a2bfc1090c140
Swarnim Bhandari <swarnimb188@gmail.com>
2018-11-16 22:10:36 -0500

    Asking the user for input and storing the input. Also creating another one to store the four characters

--- project2.s
+++ project2.s
@@ -3,3 +3,10 @@
     Input_isLong: .asciiz "Input is too long."
     emptyInput: .asciiz "Input is empty."
     Input_isInvalid: .asciiz "Invalid base-35 number."
+    # creating space for user to input
+    Input_askUser: .space 2000
+# storing the first four non-space characters. If the input has less than 4 characters, then the null value will be stored
+    users_inputstorage: .space 4
+.text
+
+main:
