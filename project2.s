b946e1707a35aea28db628c6ee86138ac95fffec
Swarnim Bhandari <swarnimb188@gmail.com>
2018-11-16 22:07:30 -0500

    This is the start of the mips code. Initialyzing strings for output

---
+++ project2.s
@@ -0,0 +1,5 @@
+.data
+#creating output in the case the input is wrong
+    Input_isLong: .asciiz "Input is too long."
+    emptyInput: .asciiz "Input is empty."
+    Input_isInvalid: .asciiz "Invalid base-35 number."
